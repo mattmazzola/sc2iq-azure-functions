c56abdc0ecd4e5e56a0df900702deca3d0ddd418
Matt Mazzola <mattmazzola@live.com>
2016-08-23 21:28:48 -0700

    Attempt to write blob buffer to local file.

--- xmlToJson/index.js
+++ xmlToJson/index.js
@@ -1,6 +1,16 @@
-module.exports = function (context, myBlob) {
-    context.log('Node.js blob trigger function processed blob:', myBlob);
-    console.log('Edited');
-    console.log(myBlob);
+var fs = rquire('fs');
+
+module.exports = function (context, xmlZipBlob) {
+    context.log('Node.js blob trigger function processed blob:', xmlZipBlob);
+    console.log(`typeof xmlZipBlob:`, typeof xmlZipBlob);
+
+    fs.writeFile('xmlZip.zip', xmlZipBlob, (err) => {
+        if (err) {
+            throw err;
+        }
+
+        console.log('saved blob to loal file called xmlZip.zip');
+    });
+
     context.done();
 };

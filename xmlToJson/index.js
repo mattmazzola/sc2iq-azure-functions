98b16feac209d93d6b44c5bd0daaea6918f44241
Matt Mazzola <mattmazzola@live.com>
2016-08-29 20:45:40 -0700

    Remove require('fs') to see if function runs again

--- xmlToJson/index.js
+++ xmlToJson/index.js
@@ -1,22 +1,22 @@
-var fs = require('fs');
+// var fs = require('fs');
 
 module.exports = function (context, xmlZipBlob) {
     context.log('Node.js blob trigger function processed blob:', xmlZipBlob);
     context.log(`typeof xmlZipBlob:`, typeof xmlZipBlob);
 
-    try {
-        fs.writeFile('xmlZip.zip', xmlZipBlob, 'utf8', (err) => {
-            if (err) {
-                throw err;
-            }
+    // try {
+    //     fs.writeFile('xmlZip.zip', xmlZipBlob, 'utf8', (err) => {
+    //         if (err) {
+    //             throw err;
+    //         }
 
-            context.log('saved blob to loal file called xmlZip.zip');
-            context.done();
-        });
-    }
-    catch (e) {
-        context.log(e);
-    }
+    //         context.log('saved blob to loal file called xmlZip.zip');
+    //         context.done();
+    //     });
+    // }
+    // catch (e) {
+    //     context.log(e);
+    // }
 
     context.log('end of function, continue execution?');
 

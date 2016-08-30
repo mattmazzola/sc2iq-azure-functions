0a7deafad5b62a1259f45e2ed51bbd4ab01b3e31
Matt Mazzola <mattmazzola@live.com>
2016-08-29 20:53:09 -0700

    Uncomment fs require

--- xmlToJson/index.js
+++ xmlToJson/index.js
@@ -1,27 +1,21 @@
-// var fs = require('fs');
+var fs = require('fs');
 
 module.exports = function (context, xmlZipBlob) {
     context.log('Node.js blob trigger function processed blob:', xmlZipBlob);
     context.log(`typeof xmlZipBlob:`, typeof xmlZipBlob);
 
-    // try {
-    //     fs.writeFile('xmlZip.zip', xmlZipBlob, 'utf8', (err) => {
-    //         if (err) {
-    //             throw err;
-    //         }
+    // fs.writeFile('xmlZip.txt', "Hello World", 'utf8', (err) => {
+    //     if (err) {
+    //         throw err;
+    //     }
 
-    //         context.log('saved blob to loal file called xmlZip.zip');
-    //         context.done();
-    //     });
-    // }
-    // catch (e) {
-    //     context.log(e);
-    // }
+    //     context.log('saved blob to loal file called xmlZip.zip');
+    //     context.done();
+    // });
 
     context.log('end of function, continue execution?');
 
     setTimeout(function () {
         context.log('timeout worked');
-        context.done();
     }, 2000);
 };

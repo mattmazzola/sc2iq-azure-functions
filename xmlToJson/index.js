3b51f6fe387777bc45b9a94c2afc43fb1782f925
Matt Mazzola <mattmazzola@live.com>
2016-08-29 20:55:58 -0700

    Uncomment test file writting

--- xmlToJson/index.js
+++ xmlToJson/index.js
@@ -4,18 +4,14 @@ module.exports = function (context, xmlZipBlob) {
     context.log('Node.js blob trigger function processed blob:', xmlZipBlob);
     context.log(`typeof xmlZipBlob:`, typeof xmlZipBlob);
 
-    // fs.writeFile('xmlZip.txt', "Hello World", 'utf8', (err) => {
-    //     if (err) {
-    //         throw err;
-    //     }
+    fs.writeFile('xmlZip.txt', "Hello World", 'utf8', (err) => {
+        if (err) {
+            throw err;
+        }
 
-    //     context.log('saved blob to loal file called xmlZip.zip');
-    //     context.done();
-    // });
+        context.log('saved blob to loal file called xmlZip.zip');
+        context.done();
+    });
 
     context.log('end of function, continue execution?');
-
-    setTimeout(function () {
-        context.log('timeout worked');
-    }, 2000);
 };

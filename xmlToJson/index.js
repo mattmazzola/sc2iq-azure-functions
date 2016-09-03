a5b130aaeb84c5dc2b26fddba87805c4d9a8915d
Matt Mazzola <mattmazzola@live.com>
2016-09-03 15:00:49 -0700

    Update xmlBlob function to write to temp directory

--- xmlToJson/index.js
+++ xmlToJson/index.js
@@ -1,15 +1,22 @@
 var fs = require('fs');
+var path = rqeuire('path');
 
 module.exports = function (context, xmlZipBlob) {
     context.log('Node.js blob trigger function processed blob:', xmlZipBlob);
     context.log(`typeof xmlZipBlob:`, typeof xmlZipBlob);
 
-    fs.writeFile('xmlZip.txt', "Hello World", 'utf8', (err) => {
-        if (err) {
-            throw err;
+    var tempDirectory = process.env["TMP"];
+    var tempFileName = "xmlZip.zip";
+    var fileDestination = path.join(tempDirectory, tempFileName);
+
+    context.log('Writing blob file to: ', fileDestination);
+
+    fs.writeFile(fileDestination, xmlZipBlob, function (error, result) {
+        if (error) {
+            throw error;
         }
 
-        context.log('saved blob to loal file called xmlZip.zip');
+        context.log('saved blob to loal file called: ', tempFileName);
         context.done();
     });
 };

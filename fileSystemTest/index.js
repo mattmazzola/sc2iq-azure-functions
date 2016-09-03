3c3d8b0e7cc15d0200561cc32ff1eef273319e04
Matt Mazzola <mattmazzola@live.com>
2016-09-03 14:01:54 -0700

    Add print out of environment variables, cwd, dirname, and change file write destination to local directory.

--- fileSystemTest/index.js
+++ fileSystemTest/index.js
@@ -1,12 +1,36 @@
 var fs = require('fs');
 
+var environmentVariables = [
+    "WEBSITE_SITE_NAME",
+    "WEBSITE_SKU",
+    "WEBSITE_COMPUTE_MODE",
+    "WEBSITE_HOSTNAME",
+    "WEBSITE_INSTANCE_ID",
+    "WEBSITE_NODE_DEFAULT_VERSION",
+    "WEBSOCKET_CONCURRENT_REQUEST_LIMIT",
+    "%APPDATA%",
+    "%TMP%"
+]
+
 module.exports = function (context, req, res) {
     context.log('function triggered');
     context.log(req);
 
-    fs.writeFile('message.txt', 'Hello Node.js', (err) => {
+    context.log('Environment Variables');
+    environmentVariables
+        .map(getEnvironmentVarible)
+        .forEach(context.log);
+
+    context.log('process.cwd()', process.cwd());
+    context.log('__dirname', __dirname);
+
+    fs.writeFile('D:/local/Temp/message.txt', 'Hello Node.js', (err) => {
         if (err) throw err;
-        console.log('It\'s saved!');
+        context.log("It's saved!");
         context.done();
     });
+}
+
+function getEnvironmentVarible(name) {
+    return name + ": " + process.env[name];
 }

5156eeb9af27e29f4b704c7293cdfffd7e34a7b7
Matt Mazzola <mattmazzola@live.com>
2016-09-18 22:12:33 -0700

    Update src with new types and update xmlToJson to use sc2-convertbalancedata functions.

--- fileSystemTest/index.ts
+++ fileSystemTest/index.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { IHttpContext, IFunctionRequest, HttpStatusCodes } from '@mattmazzola/azure-functions-typescript';
 
 const environmentVariables = [
     "WEBSITE_SITE_NAME",
--- xmlToJson/index.ts
+++ xmlToJson/index.ts
@@ -1,22 +1,54 @@
+import * as azure from "azure-storage";
 import * as fs from 'fs';
 import * as path from 'path';
+import * as cbd from 'sc2-convertbalancedata';
+import { IContext } from '@mattmazzola/azure-functions-typescript';
+import * as moment from 'moment';
 
-export function xml2json(context: any, xmlZipBlob: any) {
+const storageAccount = process.env.AZURE_STORAGE_ACCOUNT;
+const storageAccountUrl = process.env.AZURE_STORAGE_ACCOUNT_URL;
+const storageAccessKey = process.env.AZURE_STORAGE_ACCESS_KEY;
+const tempDirectory = process.env["TMP"];
+const tempFileName = "xmlZip.zip";
+const fileDestination = path.join(tempDirectory, tempFileName);
+const extractFolderName = "extract";
+const extractPath = path.join(tempDirectory, extractFolderName);
+
+const anonymousBlobService: azure.BlobService = azure.createBlobServiceAnonymous(storageAccountUrl);
+const secureBlobService = azure.createBlobService(storageAccount, storageAccessKey);
+
+export function xml2json(context: IContext, xmlZipBlob: any) {
     context.log('Node.js blob trigger function processed blob:', xmlZipBlob);
     context.log(`typeof xmlZipBlob:`, typeof xmlZipBlob);
 
-    var tempDirectory = process.env["TMP"];
-    var tempFileName = "xmlZip.zip";
-    var fileDestination = path.join(tempDirectory, tempFileName);
-
     context.log('Writing blob file to: ', fileDestination);
+    cbd.util.writeFileAsync(fileDestination, xmlZipBlob)
+        .then(() => {
+            return cbd.zipToXml(fileDestination, extractPath);
+        })
+        .then(unitsXml => {
+            const now = moment();
+            const outputBlobName = `${now.format("YYYY-MM-DD-hh-mm-ss")}.xml`;
+            return cbd.util.saveBlob(secureBlobService, "balancedataxml", outputBlobName, unitsXml)
+            .then(() => unitsXml);
+        })
+        .then(unitsXml => {
+            return cbd.xmlToJson(unitsXml);
+        })
+        .then(groupedUnits => {
+            const blobString = JSON.stringify(groupedUnits);
+            const now = moment();
+            const outputBlobName = `${now.format("YYYY-MM-DD-hh-mm-ss")}.json`;
 
-    fs.writeFile(fileDestination, xmlZipBlob, function (error, result) {
-        if (error) {
-            throw error;
-        }
-
-        context.log('saved blob to loal file called: ', tempFileName);
-        context.done();
-    });
+            return Promise.all([
+                cbd.util.writeFileAsync(path.join(tempDirectory, outputBlobName), blobString),
+                cbd.util.saveBlob(secureBlobService, "balanceddatajson", outputBlobName, blobString)
+            ]);
+        })
+        .then(() => {
+            context.done();
+        })
+        .catch(error => {
+            context.done(error);
+        });
 };
